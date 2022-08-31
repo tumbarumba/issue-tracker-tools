6131ff6e563947d7b745e43a6d0c42bde2546285
Joe Schmetzer <joe.schmetzer@penten.com>
2022-08-31 15:26:59 +1000

    Lint should echo commands

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 BASE_PATH=$(cd $(dirname $(dirname "${BASH_SOURCE:-$0}")) && pwd)
 
+# Echo commands
+set -x
+
 # Fail if there are Python syntax errors or undefined names
 flake8 $BASE_PATH --count --select=E9,F63,F7,F82 --show-source --statistics
 # exit-zero treats all errors as warnings
