a63cf248317f07ee004b6ec360b7d872d3012e8a
Joe Schmetzer <joe.schmetzer@penten.com>
2022-08-31 15:23:37 +1000

    Extract flake8 commands to script

---
+++ scripts/lint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+BASE_PATH=$(cd $(dirname $(dirname "${BASH_SOURCE:-$0}")) && pwd)
+
+# Fail if there are Python syntax errors or undefined names
+flake8 $BASE_PATH --count --select=E9,F63,F7,F82 --show-source --statistics
+# exit-zero treats all errors as warnings
+flake8 $BASE_PATH --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
