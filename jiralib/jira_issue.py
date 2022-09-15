9c8f5dcaa54c7af6c7ec975a149272bd6e761c72
Joe Schmetzer <joe.schmetzer@penten.com>
2022-09-15 11:04:17 +1000

    Fix business days when start/end out of hours

--- jiralib/jira_issue.py
+++ jiralib/jira_issue.py
@@ -132,11 +132,20 @@ def filter_by_state(stories, states_to_check):
     return list(filter(lambda story: story.fields.status.name in states_to_check, stories))
 
 
+BUSINESS_HOURS_START = 9
+BUSINESS_HOURS_END = 17
+
 def business_days(start_time, end_time):
     if not (start_time and end_time):
         return None
     bus_days = np.busday_count(start_time.date(), end_time.date())
-    bus_hours = (end_time.hour + end_time.minute / 60) - (start_time.hour + start_time.minute / 60)
+    end_hours = end_time.hour + end_time.minute / 60
+    if end_hours < BUSINESS_HOURS_START:
+        end_hours = BUSINESS_HOURS_START
+    start_hours = start_time.hour + start_time.minute / 60
+    if start_hours > BUSINESS_HOURS_END:
+        start_hours = BUSINESS_HOURS_END
+    bus_hours = end_hours - start_hours
     if bus_hours < -8:
         bus_hours = (bus_hours + 24) * -1
     if bus_hours > 8:
--- jiralib/jira_issue_test.py
+++ jiralib/jira_issue_test.py
@@ -9,7 +9,8 @@ from .jira_issue import business_days
     ("2022-03-01T10:00:00+1100", "2022-03-01T12:00:00+1100", 0.25, "Same day start/end"),
     ("2022-03-01T16:00:00+1100", "2022-03-02T10:00:00+1100", 0.25, "Skip night hours"),
     ("2022-09-06T11:00:00+1000", "2022-09-07T10:00:00+1000", 0.875, "Skip night hours"),
-    ("2022-03-01T10:00:00+1100", "2022-03-08T10:00:00+1100", 5.0, "Exactly one week")
+    ("2022-03-01T10:00:00+1100", "2022-03-08T10:00:00+1100", 5.0, "Exactly one week"),
+    ("2022-09-14T19:00:00+1000", "2022-09-15T10:00:00+1000", 0.125, "Start after hours")
 ])
 def test_business_days(start, end, expected, message):
     start = dateutil.parser.isoparse(start)
