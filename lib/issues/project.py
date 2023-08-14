380c45e782712afa7c3f1628f6aabe4d7d514f35
Joe Schmetzer <joe.schmetzer@penten.com>
2023-08-14 15:46:30 +1000

    Extract cfd_db from project report

--- lib/issues/project.py
+++ lib/issues/project.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
+
+from functools import reduce
 from typing import List, Type, TypeVar
 
 from .issue import Epic
+from .issue_counts import IssueCounts
 from .issue_provider import IssueProvider
-
 
 T = TypeVar("T", bound="Parent")  # noqa
 
@@ -12,6 +14,7 @@ class Project:
     def __init__(self: Project, project_key: str, epics: List[Epic]):
         self.key = project_key
         self.epics = epics
+        self._issue_counts = None
 
     @classmethod
     def load(cls: Type[T], issue_provider: IssueProvider, project_key: str) -> T:
@@ -23,3 +26,13 @@ class Project:
         durations = []
         # durations.extend(list(map(lambda issue: issue.duration, epic.issues)))
         return durations
+
+    @property
+    def issue_counts(self: Project) -> IssueCounts:
+        if not self._issue_counts:
+            self._issue_counts = reduce(
+                lambda accumulation, epic: accumulation + epic.issue_counts,
+                self.epics,
+                IssueCounts.zero(),
+            )
+        return self._issue_counts
--- lib/reports/report_project.py
+++ lib/reports/report_project.py
@@ -1,17 +1,9 @@
 from __future__ import annotations
 
-import csv
-from datetime import date
-from functools import reduce
-from pathlib import Path
-from typing import Any, Dict, List
-
-from dateutil.parser import isoparse
-from dateutil.relativedelta import relativedelta
+from typing import Any, List
 
 from lib.issues.issue import Epic
-from lib.issues.issue_counts import IssueCounts
-from lib.issues.issue_provider import IssueProvider
+from lib.issues.project import Project
 
 COLUMN_NAMES = ["Key", "Epic", "Pending", "In Progress", "Done", "Total"]
 COLUMN_MARGIN = 2
@@ -21,31 +13,33 @@ DEFAULT_COLUMN_WIDTHS = list(
 
 
 class ProjectReport:
-    def __init__(self: ProjectReport, issue_provider: IssueProvider, verbose: bool):
-        self.issue_provider = issue_provider
-        self.verbose: bool = verbose
+    def __init__(self: ProjectReport, project: Project):
+        self.project = project
+        self.col_widths = calculate_column_widths(project.epics)
 
-    def run(self: ProjectReport, project_key: str, csv_file: str) -> None:
-        today = str(date.today())
-
-        print(f"Project: {project_key}")
-        print(f"Date: {today}")
+    def run(self: ProjectReport, report_date: str) -> None:
+        print(f"Project: {self.project.key}")
+        print(f"Date: {report_date}")
         print()
 
-        epics = self.issue_provider.load_project_epics(project_key)
-
-        col_widths = calculate_column_widths(epics)
-        row_separator = sum(col_widths) * "="
-
-        print(format_row(COLUMN_NAMES, col_widths))
+        row_separator = sum(self.col_widths) * "="
+        print(self.format_row(COLUMN_NAMES))
         print(row_separator)
-        for epic in epics:
-            print(format_row(epic_row_values(epic), col_widths))
+        for epic in self.project.epics:
+            print(self.format_row(epic_row_values(epic)))
         print(row_separator)
-        print(format_row(total_row_values(epics), col_widths))
+        print(self.format_row(total_row_values(self.project)))
         print()
 
-        store_project_counts(today, project_key, epics, csv_file)
+    def format_row(self: ProjectReport, values: List[Any]) -> str:
+        return (
+            f"{values[0]:<{self.col_widths[0]}}"  # Key
+            f"{values[1]:<{self.col_widths[1]}}"  # Summary
+            f"{values[2]:>{self.col_widths[2]}}"  # Pending
+            f"{values[3]:>{self.col_widths[3]}}"  # In Progress
+            f"{values[4]:>{self.col_widths[4]}}"  # Done
+            f"{values[5]:>{self.col_widths[5]}}"  # Total
+        )
 
 
 def calculate_column_widths(epics) -> List[int]:
@@ -53,17 +47,6 @@ def calculate_column_widths(epics) -> List[int]:
     col_widths[0] = max(len(epic.key) for epic in epics) + COLUMN_MARGIN
     col_widths[1] = max(len(epic.summary) for epic in epics)
     return col_widths
-
-
-def format_row(values: List[Any], col_widths: List[int]) -> str:
-    return (
-        f"{values[0]:<{col_widths[0]}}"  # Key
-        f"{values[1]:<{col_widths[1]}}"  # Summary
-        f"{values[2]:>{col_widths[2]}}"  # Pending
-        f"{values[3]:>{col_widths[3]}}"  # In Progress
-        f"{values[4]:>{col_widths[4]}}"  # Done
-        f"{values[5]:>{col_widths[5]}}"  # Total
-    )
 
 
 def epic_row_values(epic: Epic) -> List[Any]:
@@ -77,94 +60,12 @@ def epic_row_values(epic: Epic) -> List[Any]:
     ]
 
 
-def total_row_values(epics: List[Epic]) -> List[Any]:
-    total_counts = accumulate_counts_for(epics)
-
+def total_row_values(project: Project) -> List[Any]:
     return [
         "",
         "",
-        total_counts.pending,
-        total_counts.in_progress,
-        total_counts.done,
-        total_counts.total,
+        project.issue_counts.pending,
+        project.issue_counts.in_progress,
+        project.issue_counts.done,
+        project.issue_counts.total,
     ]
-
-
-def accumulate_counts_for(epics) -> IssueCounts:
-    return reduce(
-        lambda accumulation, epic: accumulation + epic.issue_counts,
-        epics,
-        IssueCounts.zero(),
-    )
-
-
-def store_project_counts(
-    count_date: str, project_key: str, epics: List[Epic], csv_file: str
-) -> None:
-    csv_path = Path(csv_file)
-
-    if not csv_path.parent.exists():
-        print(f"Making directory {str(csv_path.parent)}")
-        csv_path.parent.mkdir(parents=True, exist_ok=True)
-
-    csv_data = read_csv_data(csv_path)
-    add_missing_dates(csv_data, count_date)
-    csv_data[count_date] = accumulate_counts_for(epics)
-    write_csv_data(csv_path, project_key, csv_data)
-
-
-def read_csv_data(csv_path: Path) -> Dict[str, IssueCounts]:
-    csv_data = dict()
-
-    if csv_path.exists():
-        with csv_path.open("r", encoding="UTF8") as f:
-            csv_reader = csv.DictReader(f)
-            for row in csv_reader:
-                csv_data[row["date"]] = counts_from_row(row)
-
-    return csv_data
-
-
-def counts_from_row(row: Dict[str, str]) -> IssueCounts:
-    pending = int(row["pending"])
-    in_progress = int(row["in_progress"])
-    done = int(row["done"])
-    return IssueCounts(pending, in_progress, done)
-
-
-def write_csv_data(
-    csv_path: Path, project_key: str, csv_data: Dict[str, IssueCounts]
-) -> None:
-    with csv_path.open("w", encoding="UTF8") as f:
-        field_names = ["date", "project", "pending", "in_progress", "done", "total"]
-        csv_writer = csv.DictWriter(f, fieldnames=field_names)
-        csv_writer.writeheader()
-        for key in sorted(csv_data.keys()):
-            counts = csv_data[key]
-            csv_writer.writerow(
-                {
-                    "date": key,
-                    "project": project_key,
-                    "pending": counts.pending,
-                    "in_progress": counts.in_progress,
-                    "done": counts.done,
-                    "total": counts.total,
-                }
-            )
-
-
-def add_missing_dates(csv_data: Dict[str, IssueCounts], date_to_add_str: str) -> None:
-    if not csv_data:
-        return
-
-    date_to_add = isoparse(date_to_add_str).date()
-
-    keys = sorted(csv_data.keys())
-    last_date = isoparse(keys[-1]).date()
-    while True:
-        next_date = last_date + relativedelta(days=+1)
-        if next_date >= date_to_add:
-            # No date missing
-            return
-        csv_data[str(next_date)] = csv_data[str(last_date)]
-        last_date = next_date
